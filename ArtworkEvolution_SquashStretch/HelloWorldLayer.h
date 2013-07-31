e354f9240dbd1f7b6f8bdf38979c3aab4ed825e1
Mike Lyman <mjl1309@rit.edu>
2013-07-31 16:00:47 -0400

    Initial Commit

---
+++ ArtworkEvolution_SquashStretch/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  ArtworkEvolution_SquashStretch
+//
+//  Created by Mike Lyman on 7/31/13.
+//  Copyright Mike Lyman 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
